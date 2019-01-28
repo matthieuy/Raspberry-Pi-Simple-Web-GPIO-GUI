127223dce472a95427fb76ffc0ae62e965fbffbf
Matthieu <matthieuy@riseup.net>
2019-01-28 22:12:54 +0100

    Change config

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Raspberry Pi GPIO Simple-GUI' });
+  res.render('index', { title: 'Caisson GPIO' });
 });
 
 module.exports = router;
